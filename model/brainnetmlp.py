1c7d8ee8c5d31720be9f8b756778766884a84f31
JC H <houjc24@mails.tsinghua.edu.cn>
2025-08-11 16:55:17 +0800

    upd brainnetmlp.py
    revise a mistake and add comments.

--- model/brainnetmlp.py
+++ model/brainnetmlp.py
@@ -4,11 +4,18 @@ import torch.nn as nn
 class BrainNetMLP(nn.Module):
     def __init__(self, dim, hidden_dim, drop_rate, norm, k):
         super().__init__()
+        """
+        dim: a tuple of the input dimension and output dimension
+        hidden_dim: a tuple indicates the embedding dimension of spatial and spectral features
+        drop_rate: a tuple of dropout rates for spatial and spectral projection
+        norm: bool variable to determine whether batchnorm used or not
+        k: the width of low-pass filter
+        """
         in_dim, out_dim = dim
         self.rows, self.cols = torch.triu_indices(in_dim, in_dim, offset=0)
         self.decoder = nn.Sequential(
             nn.GELU(),
-            nn.Linear(hidden_dim[1], out_dim),
+            nn.Linear(hidden_dim[0] + hidden_dim[1], out_dim),
         )
         self.freq_proj = nn.Sequential(
             nn.Linear(in_dim, hidden_dim[0]),
@@ -21,7 +28,7 @@ class BrainNetMLP(nn.Module):
         )
         self.k = k
         if norm:
-            self.norm = nn.BatchNorm1d(hidden_dim[1])
+            self.norm = nn.BatchNorm1d(hidden_dim[0] + hidden_dim[1])
         else:
             self.norm = nn.Identity()
     def forward(self, x, ts):
